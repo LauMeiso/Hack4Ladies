69c0ee182ba9262eab9d86fb8b19361c5a896826
LauraLobster <braaklaura@gmail.com>
2023-05-06 23:42:00 +0200

    added chart

--- my_first_flutter_website/lib/screens/smartphone_detail_list.dart
+++ my_first_flutter_website/lib/screens/smartphone_detail_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 import '../db/smartphone.dart';
 
@@ -25,11 +26,35 @@ class SmartphoneDetailScreen extends StatelessWidget {
                 ),
                   child: Padding(
                     padding: EdgeInsets.only(left: 0, right: 0, bottom: 0, top: 0),
-                    child: Row(children: [Image.network(smartphone.picture, width: MediaQuery.of(context).size.width / 4,)],),
-                  ),)
+                    child: Row(children: [Image.network(smartphone.picture, width: MediaQuery.of(context).size.width / 4,), SfCartesianChart(primaryXAxis: CategoryAxis(), title: ChartTitle(text: "Score Übersicht"), series: <StackedBarSeries<ChartData, String>>[
+                      StackedBarSeries<ChartData, String>(
+                      // Bind data source
+                        dataSource:  <ChartData>[
+                          ChartData('Energie', smartphone.energie),
+                          ChartData('Recycling', smartphone.recycling),
+                          ChartData('Langlebigkeit', smartphone.langlebigkeit),
+                          ChartData('Umweltverschmutzung', smartphone.umweltverschmutzung),
+                          ChartData('Soziale Verantwortung', smartphone.soziale_verantwortung),
+                          ChartData('Faire Arbeitsbedingungen', smartphone.faire_arbeitsbedingungen),
+                          ChartData('Transparenz', smartphone.transparenz)
+
+                        ],
+                        xValueMapper: (ChartData sales, _) => sales.categroy,
+                        yValueMapper: (ChartData sales, _) => sales.value
+                    )
+                  ],),
+                  ]),)
             )
         ),
       ),
-    );
+    ));
   }
 }
+
+class ChartData {
+  ChartData(this.categroy, this.value);
+  final String categroy;
+  final int value;
+}
+
+
