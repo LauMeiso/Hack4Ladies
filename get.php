74a3ca50fdb4225de73757f4bcd480f84ffda4e6
LauraLobster <braaklaura@gmail.com>
2023-05-06 07:32:53 +0200

    Create get.php

---
+++ get.php
@@ -0,0 +1,28 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "smartphones";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT name, picture FROM devices LIMIT 10";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    $rows[] = $row;
+  }
+    echo json_encode($rows);
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
