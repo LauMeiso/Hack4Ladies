b2dde6fbc568fe52519415f12c3793f0fd84f9a1
LauraLobster <braaklaura@gmail.com>
2023-05-07 00:16:56 +0200

    dummy specs in detail

--- my_first_flutter_website/lib/screens/smartphone_detail_list.dart
+++ my_first_flutter_website/lib/screens/smartphone_detail_list.dart
@@ -25,25 +25,30 @@ class SmartphoneDetailScreen extends StatelessWidget {
                   minWidth: double.infinity,
                 ),
                   child: Padding(
-                    padding: EdgeInsets.only(left: 0, right: 0, bottom: 0, top: 0),
-                    child: Row(children: [Image.network(smartphone.picture, width: MediaQuery.of(context).size.width / 4,), SfCartesianChart(primaryXAxis: CategoryAxis(), title: ChartTitle(text: "Score Übersicht"), series: <StackedBarSeries<ChartData, String>>[
-                      StackedBarSeries<ChartData, String>(
-                      // Bind data source
-                        dataSource:  <ChartData>[
-                          ChartData('Energie', smartphone.energie),
-                          ChartData('Recycling', smartphone.recycling),
-                          ChartData('Langlebigkeit', smartphone.langlebigkeit),
-                          ChartData('Umweltverschmutzung', smartphone.umweltverschmutzung),
-                          ChartData('Soziale Verantwortung', smartphone.soziale_verantwortung),
-                          ChartData('Faire Arbeitsbedingungen', smartphone.faire_arbeitsbedingungen),
-                          ChartData('Transparenz', smartphone.transparenz)
-
-                        ],
-                        xValueMapper: (ChartData sales, _) => sales.categroy,
-                        yValueMapper: (ChartData sales, _) => sales.value
-                    )
+                    padding: EdgeInsets.only(left: 0, right: 0, bottom: 0, top: 20),
+                    child: GridView.count(crossAxisCount: 3, children: [Image.network(smartphone.picture,), Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SfCartesianChart(primaryXAxis: CategoryAxis(), title: ChartTitle(text: "Score Übersicht"), series: <StackedBarSeries<ChartData, String>>[
+                        StackedBarSeries<ChartData, String>(
+                        // Bind data source
+                          dataSource:  <ChartData>[
+                            ChartData('Energie', smartphone.energie),
+                            ChartData('Recycling', smartphone.recycling),
+                            ChartData('Langlebigkeit', smartphone.langlebigkeit),
+                            ChartData('Umweltverschmutzung', smartphone.umweltverschmutzung),
+                            ChartData('Soziale Verantwortung', smartphone.soziale_verantwortung),
+                            ChartData('Faire Arbeitsbedingungen', smartphone.faire_arbeitsbedingungen),
+                            ChartData('Transparenz', smartphone.transparenz)
+                          ],
+                          xValueMapper: (ChartData sales, _) => sales.categroy,
+                          yValueMapper: (ChartData sales, _) => sales.value
+                      )
                   ],),
-                  ]),)
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 40, 0, 0),
+                    child: ListView(children: [ListTile(title: Text("Akku"), subtitle: Text("Hier specs"),), ListTile(title: Text("Mehr"), subtitle: Text("hier specs"),), ListTile(title: Text("Noch mehr"), subtitle: Text("hier specs"),)],),
+                  )]),)
             )
         ),
       ),
