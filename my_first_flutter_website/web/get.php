53f2e1975ef95ecce9959db1f8acd8f7dbb57bb2
PhoenixmitX <hauke.weinert@gmail.com>
2023-05-07 12:16:41 +0200

    docker

---
+++ my_first_flutter_website/web/get.php
@@ -0,0 +1,45 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$servername = "mysql";
+$username = "user";
+$password = "123456";
+$dbname = "database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$Auswahl = $_GET["Auswahl"];
+$GName = $_GET["GName"];
+$sql = "SELECT devices.name, devices.picture, devices.ram, devices.display_resolution, devices.battery_size, devices.released_at, devices.camera_pixels ,devices.os, devices.storage,devices.display_size, devices.battery_type, scores.Energie, scores.Recycling, scores.Langlebigkeit, scores.Umweltverschmutzung, scores.Soziale_Verantwortung, scores.Faire_Arbeitsbedingungen, scores.Transparenz  FROM devices Left Join brands ON brands.ID=devices.Brand_ID Left Join scores On brands.ID = scores.FirmenID Where devices.name Like '%{$GName}%' ORDER BY ";
+if ($Auswahl == 0)
+  $sql =   $sql . "Energie	DESC, devices.id DESC";
+else if ($Auswahl == 1)
+  $sql =   $sql . "Recycling	DESC, devices.id DESC ";
+else if ($Auswahl == 2)
+  $sql =   $sql . "Langlebigkeit	DESC, devices.id DESC ";
+else if ($Auswahl == 3)
+  $sql =   $sql . "Umweltverschmutzung	DESC, devices.id DESC ";
+else if ($Auswahl == 4)
+  $sql =   $sql . "Soziale_Verantwortung	DESC, devices.id DESC ";
+else if ($Auswahl == 5)
+  $sql =   $sql . "Faire_Arbeitsbedingungen	DESC, devices.id DESC ";
+else if ($Auswahl == 6)
+  $sql =   $sql . "Transparenz	DESC, devices.id DESC ";
+$sql =  $sql . " LIMIT ". $_GET["Lim"]." OFFSET ". $_GET["OFF"]."";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    $rows[] = $row;
+  }
+    echo json_encode($rows);
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
